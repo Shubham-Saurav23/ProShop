a4be6c3f0ed2f5491e286ae180d75896219bc452
Shubham-Saurav23 <shubhamsauravss2301@gmail.com>
2025-12-17 15:16:36 +0530

    final commit

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link, useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
 import { addToCart, removeFromCart } from '../slices/cartSlice';
-import Product from '../components/Product';
 
 const CartScreen = () => {
     const navigate = useNavigate();
